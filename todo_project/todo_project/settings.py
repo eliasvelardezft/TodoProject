f6c5b5d187d97f301e145f7e1faee8ca4803548d
elias <eliasvelardez38@gmail.com>
2021-03-24 18:19:24 -0300

    coded the frontend with functions to add, update, complete and delete tasks

--- todo_project/todo_project/settings.py
+++ todo_project/todo_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'api.apps.ApiConfig',
+    'frontend.apps.FrontendConfig',
 
     'rest_framework',
 
@@ -123,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+

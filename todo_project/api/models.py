43f7ec1e6271af7fd28accb505ef6140b3ee14ae
elias <eliasvelardez38@gmail.com>
2021-03-25 01:11:10 -0300

    I prepared the buildtasklist function to take a folder parameter in order to be able to build the task list depending on which folder was selected.

--- todo_project/api/models.py
+++ todo_project/api/models.py
@@ -10,7 +10,7 @@ class Folder(models.Model):
 class Task(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False, blank=True, null=True)
-    #folder = models.ForeignKey(Folder, on_delete=models.CASCADE)
+    folder = models.ForeignKey(Folder, on_delete=models.CASCADE, default=None)
 
     def __str__(self):
         return self.title
